384a457444a13dac3c8d7fa729ef17de19195c2b
wojdzie <wojdzi@outlook.com>
2020-08-25 18:35:06 +0200

    Spring boot dependencies and application runner have been added

---
+++ src/main/java/com/github/wojdzie/webhooks/receiver/WebhooksReceiverApplication.java
@@ -0,0 +1,12 @@
+package com.github.wojdzie.webhooks.receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhooksReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhooksReceiverApplication.class, args);
+    }
+}
